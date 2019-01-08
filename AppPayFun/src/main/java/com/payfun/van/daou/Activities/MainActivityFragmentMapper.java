76746f6f4f6865d334cc34b920d65d641396f3c7
david <david.shkim00@gmail.com>
2019-01-08 09:23:08 +0900

    add checkKeyBinding()

--- AppPayFun/src/main/java/com/payfun/van/daou/Activities/MainActivityFragmentMapper.java
+++ AppPayFun/src/main/java/com/payfun/van/daou/Activities/MainActivityFragmentMapper.java
@@ -1,5 +1,6 @@
 package com.payfun.van.daou.Activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +19,11 @@ import com.payfun.van.daou.fragments.FragmentPaymentCredit;
 import com.payfun.van.daou.fragments.FragmentPrint;
 import com.payfun.van.daou.fragments.FragmentReceipt;
 
+import ginu.android.library.utils.common.ApiDate;
+import ginu.android.library.utils.common.ApiLog;
 import ginu.android.library.utils.gui.ApiFragment;
+import ginu.android.library.utils.gui.DialogHandler;
+import ginu.android.van.app_daou.helper.AppHelper;
 import ginu.android.van.app_daou.utils.IVanString;
 
 /**
@@ -38,6 +43,10 @@ public class MainActivityFragmentMapper
 		boolean isBackStack = false;
 		String backStackTag = null;
 		mIsAtHome = false;
+
+		if( ! checkKeyBinding(activity, page) )
+			return false;
+
 		switch (page)
 		{
 			case	AMainFragPages.MainHomePage:
@@ -114,8 +123,41 @@ public class MainActivityFragmentMapper
 		return ret;
 	}
 
+	private static boolean checkKeyBinding(AppCompatActivity activity, @AMainFragPages int page)
+	{
+		String keySavedYear = AppHelper.AppPref.getKeyBindingYear();
+		String currentYear = ApiDate.getYear();
+		// ApiLog.Dbg("[MainActivityFragmentMapper]"+"keySavedYear:"+ keySavedYear + ", currentYear:"+currentYear);
+		if(keySavedYear.equals(currentYear))
+			return true;
+
+		if(page == AMainFragPages.MainHomePage)			// I don't like to show Dialog @ HOME.
+			return true;
+
+		ApiLog.Dbg("[MainActivityFragmentMapper]"+"keySavedYear:"+ keySavedYear + ", currentYear:"+currentYear);
+		ApiLog.Dbg("[MainActivityFragmentMapper]"+ WARNING_MSG_KEY_UPDATE_WARNING);
+		{
+			DialogHandler dialogHandler = new DialogHandler(activity, WARNING_KEY_UPDATE_TITLE);
+			dialogHandler.setMessage(WARNING_MSG_KEY_UPDATE_WARNING);
+			dialogHandler.setCancelable(false);
+			dialogHandler.setIcon(DialogHandler.dialogMode.MODE_WARNING);		// information Icon
+			dialogHandler.setPositiveButton(new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+				}
+			});
+			dialogHandler.Create();
+			dialogHandler.Show();
+		}
+
+		return false;
+	}
+
 	//##########################################
 	//	private Variables
 	//##########################################
+	private final static String WARNING_KEY_UPDATE_TITLE			= "키 교환 요청.";
+	private final static String WARNING_MSG_KEY_UPDATE_WARNING	= "개시거래 실행해 주세요";
+
 	private static boolean			mIsAtHome = false;
 }
